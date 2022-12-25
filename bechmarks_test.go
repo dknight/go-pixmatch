48d7e30211196229954f1c2d0d2934fc7af3acd8
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2022-12-26 00:16:51 +0200

    Added basic benchmarks.

--- bechmarks_test.go
+++ bechmarks_test.go
@@ -2,7 +2,36 @@ package pixmatch
 
 import "testing"
 
-func BenchmarkIdentical(b *testing.B) {
+var opts = DefaultOptions()
+
+func BenchmarkCompare_Empty(b *testing.B) {
+	images := []*Image{
+		NewImage(),
+		NewImage(),
+	}
+
+	for i := 0; i < b.N; i++ {
+		images[0].Compare(images[1], opts)
+	}
+}
+
+func BenchmarkCompare_Dimensions(b *testing.B) {
+	paths := []string{
+		"./res/kitten-c-small.png",
+		"./res/kitten-b.png",
+	}
+	images := make([]*Image, 0, len(paths))
+	for _, p := range paths {
+		img, _ := NewImageFromPath(p)
+		images = append(images, img)
+	}
+
+	for i := 0; i < b.N; i++ {
+		images[0].Compare(images[1], opts)
+	}
+}
+
+func BenchmarkCompare_Identical(b *testing.B) {
 	paths := []string{
 		"./res/kitten-b.png",
 		"./res/kitten-b.png",
@@ -14,6 +43,22 @@ func BenchmarkIdentical(b *testing.B) {
 	}
 
 	for i := 0; i < b.N; i++ {
-		images[0].Identical(images[1])
+		images[0].Compare(images[1], opts)
+	}
+}
+
+func BenchmarkCompare_Different(b *testing.B) {
+	paths := []string{
+		"./res/kitten-a.png",
+		"./res/kitten-b.png",
+	}
+	images := make([]*Image, 0, len(paths))
+	for _, p := range paths {
+		img, _ := NewImageFromPath(p)
+		images = append(images, img)
+	}
+
+	for i := 0; i < b.N; i++ {
+		images[0].Compare(images[1], opts)
 	}
 }
