eaebecca44276de1d834e3f4314f654e23e270f4
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2023-01-06 14:11:01 +0200

    Added Linux ARM build binary.

--- scripts/makebin.sh
+++ scripts/makebin.sh
@@ -9,6 +9,7 @@ mkdir -pv "$BINDIR"
 echo "Compiling..."
 
 GOOS=linux GOARCH=amd64 go build -o "$BINDIR/$CMDNAME-amd64-linux"
+GOOS=linux GOARCH=arm64 go build -o "$BINDIR/$CMDNAME-arm64-linux"
 GOOS=darwin GOARCH=amd64 go build -o "$BINDIR/$CMDNAME-amd64-darwin"
 GOOS=darwin GOARCH=arm64 go build -o "$BINDIR/$CMDNAME-arm64-darwin"
 GOOS=windows GOARCH=amd64 go build -o "$BINDIR/$CMDNAME.exe"
